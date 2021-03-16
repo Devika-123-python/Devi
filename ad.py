05af80d2f88f21037c998f0f65cad46f0c87f64f
Devika-123-python/pythongit config --global user.email vaddidevi06@gmail.com <vaddidevi06@gmail.com>
2021-03-16 18:44:56 +0530

    ad

--- ad.py
+++ ad.py
@@ -1,1 +1,13 @@
-print("ad")
+# a = ("a", "b", "c", "d", "e", "f", "g", "h")
+# x = slice(2)
+# print(a[x])
+
+#global variable declaration
+f = 101;
+print(f)
+# Global vs.local variables in functions
+def someFunction():
+ print(f)
+f = "changing global variable"
+someFunction()
+print(f)
