ef1fbccd8afa90a5041535f24e1868dbf55f055f
Devika-123-python/pythongit config --global user.email vaddidevi06@gmail.com <vaddidevi06@gmail.com>
2021-03-28 12:54:22 +0530

    append

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("hii")
+# Dict = {'Tim': 18,'Charlie':12,'Tiffany':22,'Robert':25}
+# print("variable Type: %s" %type (Dict))
+Dict = {'Tim': 18,'Charlie':12,'Tiffany':22,'Robert':25}
+print("printable string:%s" % str (Dict))
